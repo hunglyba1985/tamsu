64e912c6d489e0b913b17e9bd2f320265a071f23
hunglyba1985 <hunglb.ctv@gmail.com>
2018-03-07 11:50:39 +0700

    Initial Commit

---
+++ Tam_su/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tam_su
+//
+//  Created by MacOS on 3/7/18.
+//  Copyright © 2018 MacOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tam_su/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tam_su
+//
+//  Created by MacOS on 3/7/18.
+//  Copyright © 2018 MacOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

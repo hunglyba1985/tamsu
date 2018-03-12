37e0052b0d32bd5ba3b3482aec42c39b040b39ac
hunglb1985 <hunglb.ctv@gmail.com>
2018-03-12 16:22:28 +0700

    Add chat view controller

---
+++ Tam_su/Controller/ChatViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChatViewController.h
+//  Tam_su
+//
+//  Created by MacOS on 3/12/18.
+//  Copyright © 2018 MacOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JSQMessages.h"
+
+@interface ChatViewController : JSQMessagesViewController
+
+@end
